a0b51fe94a6fe644266d1560930c8ba2f643ae12
llebacq <llebacq@vistalid.fr>
2023-06-01 16:17:27 +0200

    [WIP]: Add user provider

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Quiz from './quiz';
--- front/src/quiz.js
+++ front/src/quiz.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
 function Quiz() {
-  const [quizs, showQuiz] = useState([]);
+    const [quizs, showQuiz] = useState([]);
 
-  useEffect(() => {
-    axios.get('/quiz')
-      .then(response => {
-        console.log(response.data);
-        showQuiz(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [])
+    useEffect(() => {
+        axios.get('/quiz')
+            .then(response => {
+                console.log(response.data);
+                showQuiz(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [])
 
-  return (
-    <>
-      <h3>Quiz</h3>
-      <div>
-        {quizs.map((quiz) => (
-          <div key={quiz.id}>
-            <h2>{quiz.season}</h2>
-            <h3>{quiz.title}</h3>
-            <p>{quiz.description}</p>
-            <img src={`http://127.0.0.1:8000/upload/images/quiz/${quiz.image}`} alt={quiz.title} />
-          </div>
-        ))}
-        <button><a href="./questions" class="button">Commencer</a></button>
-      </div>
-    </>
-  );
+    return (
+        <div>
+            <h3>Quiz</h3>
+            <div>
+                {quizs.map((quiz) => (
+                    <div key={quiz.id}>
+                        <h2>{quiz.season}</h2>
+                        <h3>{quiz.title}</h3>
+                        <p>{quiz.description}</p>
+                        <img src={`http://127.0.0.1:8000/upload/images/quiz/${quiz.image}`} alt={quiz.title} />
+                    </div>
+                ))}
+                <button><a href="./questions" class="button">Commencer</a></button>
+            </div>
+        </div>
+    );
 }
 
 export default Quiz;
